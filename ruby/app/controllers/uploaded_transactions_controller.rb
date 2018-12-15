b4d9732148827ad704ff961800d256399ceb6b18
Lakshmi <lraghavan@salesforce.com>
2018-12-14 20:15:40 -0800

    used material design for form, dataTable and ui/ux

--- ruby/app/controllers/uploaded_transactions_controller.rb
+++ ruby/app/controllers/uploaded_transactions_controller.rb
@@ -4,14 +4,21 @@ class UploadedTransactionsController < ApplicationController
     @uploaded_transaction = @agent.uploaded_seller_transactions.new
   end
 
+  #Before creating a transaction record check if if all the params are present. 
+  #IF the values are missing the log
+  #If present try saving, when that is successfull redirect to agents page
+  # else try again
   def create
     agent = Agent.find(params[:agent_id])
-    uploaded_transaction = agent.uploaded_seller_transactions.create(uploaded_transaction_params)
-
-    if uploaded_transaction.save
-      redirect_to agent_path(agent), notice: "Transaction saved!"
-    else
+    if (checkParamValues(uploaded_transaction_params))
+      uploaded_transaction = agent.uploaded_seller_transactions.create(uploaded_transaction_params)        
+      if uploaded_transaction.save
+        redirect_to agent_path(agent), notice: "Transaction saved!"
+      else
       render "new"
+      end
+    else
+      Rails.logger.debug(json: {status: "error", code: 3000, message: "Mandatory fields were not filled"})
     end
   end
 
@@ -20,4 +27,14 @@ class UploadedTransactionsController < ApplicationController
   def uploaded_transaction_params
     params.require(:uploaded_transaction).permit(:address, :city, :state, :zip, :listing_agent, :listing_price, :listing_date, :selling_price, :selling_agent, :selling_date, :status, :property_type)
   end
+
+  def checkParamValues(uploaded_transaction_params)
+    params.has_key?(:address) && params.has_key?(:city) &&
+      params.has_key?(:state) && params.has_key?(:zip) &&
+      params.has_key?(:listing_agent) && params.has_key?(:listing_price) &&
+      params.has_key?(:listing_date) && params.has_key?(:selling_price) &&
+      params.has_key?(:selling_date) && params.has_key?(:status) &&
+      params.has_key?(:property_type)
+  end
+
 end
