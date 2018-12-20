09e2e723f328f705d6304fa7c8fc389362c9738c
Lakshmi Narasimhan <rajulonline@gmail.com>
2018-12-19 23:54:25 -0800

    adding simple global search using elasticsearch, changed the default number of returned search results from 10 to 1000

--- ruby/config/routes.rb
+++ ruby/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "agents#random_agent"
 
   match '/bulkuploadcsv', to: 'agents#bulkuploadcsv',via: [:get, :post]
+  match '/uploaded_transaction/search', to: 'uploaded_transactions#search', via: [:get, :post]
   post '/bulkTransactionsUploader', to: 'agents#bulkTransactionsUploader'
 
   resources :agents do
